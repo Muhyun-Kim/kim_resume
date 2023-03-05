811872541e916d5bba42298bf18f5ce7b4ceece7
Muhyun-Kim <azs030369@gmail.com>
2023-03-05 19:57:08 +0900

    add line account

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faTwitter,
+  faLine,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function Footer() {
+  const lineClick = () => {
+    window.open("https://line.me/ti/p/JqbdHauzqx");
+  };
   const gitHubClick = () => {
     window.open("https://github.com/Muhyun-Kim");
   };
@@ -14,11 +22,16 @@ function Footer() {
     <>
       <ul className="flex justify-between w-11/12 h-24 px-4 border-t-2 items-center fixed bottom-0 z-10 bg-gray-100">
         <li className="flex flex-col items-center">
-          <FontAwesomeIcon icon={faPhone} />
-          <p>080-8858-2858</p>
+          <FontAwesomeIcon
+            icon={faLine}
+            className="text-2xl"
+            onClick={lineClick}
+          />
         </li>
         <li className="flex flex-col items-center">
-          <FontAwesomeIcon icon={faEnvelope} />
+          <Link to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </Link>
           <p>muhyun.kim.dev@gmail.com</p>
         </li>
         <li>
